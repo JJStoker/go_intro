64b13d9f35d71597f6405994b6f775a7f9e848ae
JJStoker <jeroen229@gmail.com>
2020-08-23 12:05:12 +0200

    Add chapter 2 notes

---
+++ ch2.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+	// fmt.Println("1 + 1 =", 1.0+1.0)
+
+	// fmt.Println(len("Hello, World"))
+	// fmt.Println("Hello, World"[1])
+	// fmt.Println(string("Hello, World"[1]))
+	// fmt.Println("Hello, " + "World")
+
+	// fmt.Println(true && true)
+	// fmt.Println(true && false)
+	// fmt.Println(true || true)
+	// fmt.Println(true || false)
+	// fmt.Println(!true)
+
+	// Exercise 3
+	fmt.Println(31.132 * 42.452)
+
+	// Exercise 5
+	fmt.Println((true && false) || (false && true) || !(false && false))
+}
+
+/*
+Exercises
+1: as bits, using various sizes like 8bit / 32bit / 64bit
+2: 11111111 (255)
+3: see main() -> 1321.615664
+4: sequence of characters that represent text
+5: see main() -> true
+*/
